f55eb94052e2ef7a49c2c58ae72e75c69edba69e
Md Aminul Islam <mr.aminul118@gmail.com>
2025-01-22 00:18:39 +0600

    Root Layout create and Navbar, Footer component create

---
+++ src/app/(root)/layout.jsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+const layout = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      <div className="min-h-[calc(100vh-292px)] container mx-auto px-6">
+        {children}
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default layout;
